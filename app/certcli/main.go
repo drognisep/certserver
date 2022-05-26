a39ae00fea75433dcea612eca9f40feeb2d88af5
Doug Saylor <doug@saylorsolutions.com>
2022-05-26 00:28:07 -0500

    Adding help text to top level command

--- app/certcli/main.go
+++ app/certcli/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
@@ -10,8 +9,32 @@ type cliCommand func(command string, args []string)
 
 func main() {
 	args := os.Args[1:]
+	usage := func() {
+		fmt.Print(`certcli is a helpful CLI application to generate TLS artifacts, like root CA
+certs, CSRs, client and server certs.
+See the command listing below for more details.
+
+Usage: certcli COMMAND [FLAGS]... [ARGS]...
+
+COMMAND:
+  root-ca:   Generates a root, self-signed CA.
+  cert-info: View the details of a PEM or DER encoded certificate.
+  csr:       Generate a CSR with provided details.
+  sign:      Sign a CSR with a given CA cert and key and create a client cert, server cert, or sub-CA.
+  format:    Change the encoding of a certificate or private key between PEM and DER.
+
+See each command's help text for more info.
+`)
+	}
+
 	if len(args) == 0 {
-		log.Fatalln("No arguments specified")
+		fmt.Println("No arguments specified")
+		usage()
+		os.Exit(1)
+	}
+	if args[0] == "--help" || args[0] == "-h" {
+		usage()
+		return
 	}
 
 	cmdMap := map[string]cliCommand{
